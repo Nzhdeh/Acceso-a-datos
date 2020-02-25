a253d152f0ddb812465ec129fcc11dc71b1ab987
Nzhdeh <nzhdeh1991@gmail.com>
2020-02-25 11:47:15 +0100

    dyrt

---
+++ Hibernate/src/clases/Disco.java
@@ -0,0 +1,74 @@
+package clases;
+
+import java.io.Serializable;
+
+//import org.hibernate.annotations.Entity;
+import javax.persistence.*;
+
+@Entity
+@Table(name="Discos")
+public class Disco implements Serializable
+{
+	@Id
+	@Column(name="id")
+	private int id;
+	@Column(name="autor")
+	private String autor;
+	@Column(name="titulo")
+	private String titulo;
+	@Column(name="formato")
+	private String formato;
+	@Column(name="localizacion")
+	private String localizacion;
+	
+	public Disco() 
+	{
+		this.id = 0;
+		this.autor = "";
+		this.titulo = "";
+		this.formato = "";
+		this.localizacion = "";
+	}
+	
+	public Disco(int id, String autor, String titulo, String formato, String localizacion) 
+	{
+		this.id = id;
+		this.autor = autor;
+		this.titulo = titulo;
+		this.formato = formato;
+		this.localizacion = localizacion;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getFormato() {
+		return formato;
+	}
+	public void setFormato(String formato) {
+		this.formato = formato;
+	}
+	public String getLocalizacion() {
+		return localizacion;
+	}
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
+	
+	
+}
---
+++ Hibernate/src/main/Principal.java
@@ -0,0 +1,11 @@
+package main;
+
+import clases.Disco;
+
+public class Principal 
+{
+	public static void main(String[] args) 
+	{
+		Disco d=new Disco();
+	}
+}
